6e4e173b37788f8a2691d2a65c00aa6af0e397e3
Abriam-Michaela <abriammichaelajoan36@gmail.com>
2024-04-15 14:03:11 +0800

    Homepage

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to our store</h1>
+      <p>We offer a wide range of products. Click "Proceed to Shopping" to start browsing.</p>
+    </div>
+  );
+}
+
+export default Home;
