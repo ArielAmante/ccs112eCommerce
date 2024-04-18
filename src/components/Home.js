fc816f4abf76c1652430a096e2ab86b8e3d614f3
Abriam-Michaela <abriammichaelajoan36@gmail.com>
2024-04-18 13:56:40 +0800

    Add Logo (Homepage)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 // Home.js
 import React from 'react';
+import './Homepage.css';
+import logo from './images/Logo.png';
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to our store</h1>
+      <div className="logo">
+        <img src={logo} alt="Logo"/>
+      </div>
+
+      <h1>Welcome to Shimmer Store!</h1>
       <p>We offer a wide range of products. Click "Proceed to Shopping" to start browsing.</p>
     </div>
   );
