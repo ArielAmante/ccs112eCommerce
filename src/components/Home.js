d1ea87728fdd572b389a6a8f1edd4ccb8ef766f0
ArielAmante <amanteariel660@gmail.com>
2024-04-19 15:01:48 +0800

    add images for products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,19 @@
 // Home.js
 import React from 'react';
 import './Homepage.css';
-import logo from './images/Logo.png';
+import logo from '../images/Logo.png'; // Update the logo import path
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <div className="logo">
         <img src={logo} alt="Logo"/>
       </div>
 
-      <h1>Welcome to Shimmer Store!</h1>
-      <p>We offer a wide range of products. Click "Proceed to Shopping" to start browsing.</p>
+      <div className="content">
+        <h1>Welcome to Shimmer Store!</h1>
+        <p>We offer a wide range of products. Click "Proceed to Shopping" to start browsing.</p>
+      </div>
     </div>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,22 +1,32 @@
 // ProductList.js
 import React from 'react';
-// import Product from './Product'; // Remove or comment out the import statement
+import yellowPad from '../images/yellow_pad.jpg';
+import scissors from '../images/scissors.jpg';
+import longBondPaper from '../images/long_bond_paper.jpg';
+import shortBondPaper from '../images/short_bond_paper.jpg';
+import blackBallpen from '../images/black_ballpen.jpg';
+import blueBallpen from '../images/blue_ballpen.jpg';
+import highlighter from '../images/highlighter.jpg';
+import assortedCartolina from '../images/assorted_cartolina.jpg';
+import assortedArtPaper from '../images/assorted_art_paper.jpg';
+import plasticCover from '../images/plastic_cover.jpg';
+import pencilSharpener from '../images/pencil_sharpener.jpg';
+import calculator from '../images/calculator.jpg';
 
 const ProductList = ({ addToCart }) => {
   const products = [
-    { id: 1, name: 'Intermediate Pad', description: '1 whole', price: 10, image: 'intermediate_pad.jpg' },
-    { id: 2, name: '1 pair of Scissors', description: 'HBW', price: 15, image: 'scissors.jpg' },
-    { id: 3, name: 'Long Bond Paper', description: 'Orion', price: 30, image: 'long_bond_paper.jpg' },
-    { id: 4, name: 'Short Bond Paper', description: 'Orion', price: 25, image: 'short_bond_paper.jpg' },
-    { id: 5, name: '1 pc Black Ballpen', description: 'G-Tech', price: 5, image: 'black_ballpen.jpg' },
-    { id: 6, name: '1 pc. Blue Ballpen', description: 'Panda', price: 8, image: 'blue_ballpen.jpg' },
-    { id: 7, name: '1 pc Manila Paper', description: 'with border', price: 25, image: 'manila_paper.jpg' },
-    { id: 8, name: 'Assorted Cartolina', description: 'with border', price: 25, image: 'assorted_cartolina.jpg' },
-    { id: 9, name: 'Assorted Art Paper', description: '1 rim', price: 25, image: 'assorted_art_paper.jpg' },
-    { id: 10, name: 'Plastic Cover', description: '2ft', price: 16, image: 'plastic_cover.jpg' },
-    { id: 11, name: 'Pencil Sharpener', description: 'Manual', price: 3, image: 'pencil_sharpener.jpg' },
-    { id: 12, name: 'Eraser', description: 'Rubber', price: 2, image: 'eraser.jpg' },
-    { id: 13, name: 'Calculator', description: 'Scientific', price: 50, image: 'calculator.jpg' },
+    { id: 1, name: 'Yellow Pad Paper', description: '1 pad', price: 26, image: yellowPad },
+    { id: 2, name: '1 pair of Scissors', description: 'HBW', price: 13, image: scissors },
+    { id: 3, name: 'Long Bond Paper', description: 'Orion', price: 30, image: longBondPaper },
+    { id: 4, name: 'Short Bond Paper', description: 'Orion', price: 28, image: shortBondPaper },
+    { id: 5, name: '1 pc Black Ballpen', description: 'G-Tech', price: 65, image: blackBallpen },
+    { id: 6, name: '1 pc. Blue Ballpen', description: 'Panda', price: 8, image: blueBallpen },
+    { id: 7, name: '1 pc Highlighter', description: 'Yellow', price: 26, image: highlighter },
+    { id: 8, name: 'Assorted Cartolina', description: 'with border', price: 13, image: assortedCartolina },
+    { id: 9, name: 'Assorted Art Paper', description: '1 rim', price: 11, image: assortedArtPaper },
+    { id: 10, name: 'Clear Plastic Cover', description: '2ft', price: 16, image: plasticCover },
+    { id: 11, name: 'Pencil Sharpener', description: 'Manual', price: 3, image: pencilSharpener },
+    { id: 12, name: 'Scientific Calculator', description: 'Scientific', price: 50, image: calculator }
   ];
 
   return (
@@ -26,7 +36,7 @@ const ProductList = ({ addToCart }) => {
         {products.map(product => (
           <li key={product.id} className="product-item">
             <div className="product-details">
-            <img src={require(`./images/${product.image}`).default} alt={product.name} className="product-image" />
+              <img src={product.image} alt={product.name} className="product-image" style={{ maxWidth: '100%', maxHeight: '100%' }} />
               <h3 className="product-name">{product.name}</h3>
               <p className="product-price">₱ {product.price}</p>
               <button onClick={() => addToCart(product)} className="add-to-cart-button">Add to Cart</button>
